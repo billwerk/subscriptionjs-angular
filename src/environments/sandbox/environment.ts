4badeb149d9801e95d845a71cbf68d7efae5432c
AxelDuenninger <axel@billwerk.com>
2021-05-10 10:53:53 +0200

    security updates

--- src/environments/sandbox/environment.ts
+++ src/environments/sandbox/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   publicApiKey: '5981a1e381b1fc126071dc3c',
-  clientId: '5dc27bb84de0841d6c8a43b4',
-  clientSecret: 'a6fb25b2300772c9b25195331153c31f'
+  clientId: '6098f3dab1746290772222e0',
+  clientSecret: '75bf47bf39ba40c9b00d97d8a1c1aef8'
 };
